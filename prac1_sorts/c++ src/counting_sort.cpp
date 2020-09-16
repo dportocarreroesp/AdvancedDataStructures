5dd779030aea5ff3f52526f943722aba7ae16673
kpzaolod6000 <43651853+kpzaolod6000@users.noreply.github.com>
2020-09-16 08:22:37 -0500

    Add files via upload

---
+++ prac1_sorts/c++ src/counting_sort.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+#define max 10
+
+int* counting_sort(int* array,int* new_array){
+    int num_max = array[0], num_min = array[0];
+    for (int i = 1; i < max; i++)
+    {
+        if(array[i] > num_max)
+            num_max = array[i];
+        if(array[i] < num_min)
+            num_min = array[i];
+    }
+    int count[num_max+1]={0};
+    for (int j=0; j < max ;j++ )
+    {  
+        count[array[j]]++;
+    }
+
+    for (int i=0; i < num_max+1 ;i++ )
+    {  
+        count[i+1] = count[i] + count[i+1];
+    }
+    for (int i = 0; i < max; i++)
+    {
+        new_array[count[array[i]]-1] = array[i];
+        count[array[i]]--;
+    }
+    return new_array;
+
+}
+
+void print(int* new_array){
+     for(int i=0;i<max;i++){
+        cout<<new_array[i]<<" ";
+    }
+}
+int main(){
+
+    int num;
+    int array[max];
+    for (int i = 0; i < max; i++)
+    {
+        cin>>num;
+        array[i] = num;
+        cout<<array[i] <<" ";
+    }
+    cout<<endl;
+    
+    //Counting sort
+    int aux[max];
+    int* new_array = counting_sort(array,aux);
+
+    //print  
+    cout<<endl;
+    print(new_array);
+   
+    cout<<endl;
+    return 0;
+
+}

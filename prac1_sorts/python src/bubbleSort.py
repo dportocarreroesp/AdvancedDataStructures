6ab81c43913cf61418fc51493cf5e064e8e10493
Diego Armando Portocarrero Espirilla <despearux@gmail.com>
2020-09-16 22:16:34 -0500

    quick y bubble en python y java

---
+++ prac1_sorts/python src/bubbleSort.py
@@ -0,0 +1,21 @@
+def bubble_sort(arr, n):
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if(arr[j] > arr[j+1]):
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+def printArray(arr, n):
+    for i in range(n):
+        print(arr[i], end=" ")
+    print()
+
+
+arr = []
+n = int(input())
+
+for i in range(n):
+    arr.append(int(input()))
+
+bubble_sort(arr,n)
+
+printArray(arr,n)

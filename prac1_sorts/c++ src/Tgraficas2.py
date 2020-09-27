c0a8c22143d1188fbbfecf48c8e3d274c7f2e0fd
artal23 <artal23jara@gmail.com>
2020-09-27 17:06:02 -0500

    corregido

--- prac1_sorts/c++ src/Tgraficas2.py
+++ prac1_sorts/c++ src/Tgraficas2.py
@@ -9,8 +9,8 @@ for chunk in datos:
 	x = list(chunk.iloc[:,0])
 	y1 = list(chunk.iloc[:,1])
 	y2 = list(chunk.iloc[:,2])
-	y3 = list(chunk.iloc[:,3])
-	y4 = list(chunk.iloc[:,4])
+	y3 = list(chunk.iloc[:,4])
+	y4 = list(chunk.iloc[:,5])
 
 #	print(x)
 #	print(y)
@@ -20,8 +20,8 @@ print(y1)
 print(y2)
 print(y3)
 print(y4)
-plot(x,y1,'bp-',x,y2,'rd-',x,y4,'m<-')
-legend(('couting','heap','quick'),prop = {'size':10},loc='upper left')
+plot(x,y1,'bp-',x,y2,'rd-',x,y3,'go-',x,y4,'m<-')
+legend(('couting','heap','merge','quick'),prop = {'size':10},loc='upper left')
 '''
 x,y3,'go-',
 ,'merge'

031e5d676c817ddb06a550e660bb2c2fae7b98b2
artal23 <artal23jara@gmail.com>
2020-09-27 13:44:41 -0500

    graficas actualizadas

--- prac1_sorts/c++ src/Tgraficas2.py
+++ prac1_sorts/c++ src/Tgraficas2.py
@@ -9,11 +9,8 @@ for chunk in datos:
 	x = list(chunk.iloc[:,0])
 	y1 = list(chunk.iloc[:,1])
 	y2 = list(chunk.iloc[:,2])
-	#y3 = list(chunk.iloc[:,3])
+	y3 = list(chunk.iloc[:,3])
 	y4 = list(chunk.iloc[:,4])
-	#y5 = list(chunk.iloc[:,5])
-	#y6 = list(chunk.iloc[:,6])
-	#y7 = list(chunk.iloc[:,7])
 
 #	print(x)
 #	print(y)
@@ -21,21 +18,24 @@ for chunk in datos:
 print(x)
 print(y1)
 print(y2)
-#print(y3)
+print(y3)
 print(y4)
-#print(y5)
-#print(y6)
-#print(y7)
-
 plot(x,y1,'bp-',x,y2,'rd-',x,y4,'m<-')
-legend(('couting','heap','merge'),prop = {'size':10},loc='upper left')
+legend(('couting','heap','quick'),prop = {'size':10},loc='upper left')
+'''
+x,y3,'go-',
+,'merge'
+for i in range(6):
+	pt=np.arange(x,ylabel)
+#x = arange(3,40)
+#y = arange(3,40)
+#plt.subplot(3,2,1()
+	plt.plot(pt)    
+	plt.xlabel('elementos')
+	plt.ylabel('tiempo')
+	plt.title('Comparacion Algoritmos')
+	plt.grid(True)
+'''
 
 
 show()
-'''
-,x,y3,'go-'
-,x,y5,'y>-',x,y6,'ko-',x,y7,'cp-'
-
-'insertion',,'quick','selection','buble'
-
-'''

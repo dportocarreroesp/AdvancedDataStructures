36c85da5fcbdd8263c11f7c834ea0bb5e13d7b70
Diego Armando Portocarrero Espirilla <despearux@gmail.com>
2020-10-13 22:14:04 -0500

    java bubble

--- prac1_sorts/java src/BubbleSort.java
+++ prac1_sorts/java src/BubbleSort.java
@@ -35,13 +35,15 @@ class BubbleSort {
     { 
         int i, j; 
         int temp;
-        for (i = 0; i < n-1; i++)
-            for (j = 0; j < n-i-1; j++) 
+        for (i = 0; i < n-1; i++) {
+            for (j = 0; j < n-i-1; j++) {
                 if (arr[j] > arr[j+1]) 
                 {
                     temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
+            }
+        }
     }
 }

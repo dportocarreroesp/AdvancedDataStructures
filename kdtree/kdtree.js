d4fc48a07aad12045d4454df297fa9622b3cba69
artal <artal23jara@gmail.com>
2020-11-04 18:18:31 -0500

    kd

---
+++ kdtree/kdtree.js
@@ -0,0 +1,28 @@
+k=2;
+
+class Node{
+constructor(point,axis){
+this.point = point;
+this.left = null;
+this.right = null;
+this.axis = axis;
+}
+}
+
+function getHeight(node){}
+function generate_dot(node){}
+function build_kdtree(points, depth = 0){}
+
+digraph G{
+
+	"106,189" -> "6,114";
+	"6,114" -> "90,102";
+	"90,102" -> "21,84";
+	"6,114" -> "84,138";
+	"84,138" -> "5,150";
+	"106,189" -> "148,85";
+	"148,85" -> "181,45";
+	"181,45" -> "161,29";
+	"148,85" -> "158,120";
+}
+

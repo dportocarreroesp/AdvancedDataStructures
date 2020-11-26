4a8e30359b3e2b5316d075147efd7bdea142b43f
gcoayla <gcoayla.28@gmail.com>
2020-11-25 22:37:17 -0500

    Mejoras

--- Kdtree/prepro.py
+++ Kdtree/prepro.py
@@ -1,5 +1,8 @@
+import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from collections import Counter
+from _collections import OrderedDict
 import csv
 stop_words = stopwords.words('english')
 stop_words +='?';
@@ -23,7 +26,18 @@ stop_words +="'";
 stop_words +="''";
 stop_words +='!';
 stop_words +='$';
-
+stop_words +='I';
+stop_words +='p';
+stop_words +='/p';
+stop_words +='It';
+stop_words +='%';
+stop_words +='+';
+stop_words +='Is';
+stop_words +='--';
+stop_words +='&';
+stop_words +="'s";
+stop_words +="How";
+stop_words +="how";
 with open('train.csv', encoding="utf8") as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
@@ -34,6 +48,7 @@ with open('train.csv', encoding="utf8") as csvfile:
         for w in word_tokens:
             if w not in stop_words:
                 filtered_sentence.append(w)
+        c=Counter(filtered_sentence)
 
         word_tokens2 = word_tokenize(row['Body'])
         filtered_sentence2 = [w for w in word_tokens2 if not w in stop_words]
@@ -41,5 +56,6 @@ with open('train.csv', encoding="utf8") as csvfile:
         for w in word_tokens2:
             if w not in stop_words:
                 filtered_sentence2.append(w)
-        print(filtered_sentence)
-        print(filtered_sentence2)
+        c2 = Counter(filtered_sentence2)
+        print(c.most_common(5))
+        print(c2.most_common(5))
